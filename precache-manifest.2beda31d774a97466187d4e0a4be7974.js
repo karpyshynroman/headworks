bf7fa212ae573f47dd41b7456dda08fd0fcaa387
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-11-28 22:43:29 +0200

    Updates

---
+++ precache-manifest.2beda31d774a97466187d4e0a4be7974.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98132d13e9a377e63b87f1629c2e2022",
+    "url": "/headworks/index.html"
+  },
+  {
+    "revision": "efa0bfa93c83bdb708e2",
+    "url": "/headworks/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "46ecc995a4e88e3be7a1",
+    "url": "/headworks/static/css/main.48dea849.chunk.css"
+  },
+  {
+    "revision": "efa0bfa93c83bdb708e2",
+    "url": "/headworks/static/js/2.e07e5c13.chunk.js"
+  },
+  {
+    "revision": "46ecc995a4e88e3be7a1",
+    "url": "/headworks/static/js/main.1381a7c9.chunk.js"
+  },
+  {
+    "revision": "222feceaa4e9af8de64a",
+    "url": "/headworks/static/js/runtime-main.a21cd548.js"
+  },
+  {
+    "revision": "c0de32fd6f2a81a44e3448d8b489989c",
+    "url": "/headworks/static/media/dia.c0de32fd.jpg"
+  },
+  {
+    "revision": "fe545f3dfe3a94ee8b2a145c3e52ab12",
+    "url": "/headworks/static/media/miamy.fe545f3d.jpg"
+  },
+  {
+    "revision": "e1fb62ff0a39bd922724bd0e017941a1",
+    "url": "/headworks/static/media/todo.e1fb62ff.jpg"
+  },
+  {
+    "revision": "4e7f39d9165685b27d4df4ae3ddb5dfb",
+    "url": "/headworks/static/media/wheelshop.4e7f39d9.jpg"
+  }
+]);
